b0ba08db65fdff293b21a2d104ce16f7da5d7d86
AbsoluteMassive <absolutemassive@duck.com>
2026-03-04 13:45:09 +0000

    Rename variables in forWater function

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,11 @@
 function forWater() {
-  var paragraph = document.getElementById("carrot");
-      paragraph.textContent = "this is a new paragraph";
+  var paragraphChange = document.getElementById("carrot");
+      paragraphChange.textContent = "this is a new paragraph";
 
-  var heading = document.getElementById("banana");
-      heading.textContent = "JavaScript test page";
+  var headingChange = document.getElementById("banana");
+      headingChange.textContent = "JavaScript test page";
 };
 
 /* var x = 5; var y = 12; var result = x + y; console.log(result); */
 // COMMENT 
+
