b786904f49303732f1fbebc2d18476d8e4fc4557
AbsoluteMassive <absolutemassive@duck.com>
2026-03-04 13:30:49 +0000

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+function forWater() {
+  var paragraph = document.getElementById("carrot");
+      paragraph.textContent = "this is a new paragraph";
+
+  var heading = document.getElementById("banana");
+      heading.textContent = "JavaScript test page";
+};
+
+/* var x = 5; var y = 12; var result = x + y; console.log(result); */
+// COMMENT 
